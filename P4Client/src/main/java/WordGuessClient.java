44afeacf46e37515bec5363d683fe6f4e5024c25
Ronaldo Miguel <miguel.ronaldo1809@gmail.com>
2020-04-25 17:26:12 -0500

    added a start screen

--- P4Client/src/main/java/WordGuessClient.java
+++ P4Client/src/main/java/WordGuessClient.java
@@ -1,9 +1,27 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.util.HashMap;
 
 public class WordGuessClient extends Application {
+
+	TextField portBox;
+	Text portText;
+	TextField ipBox;
+	Text ipText;
+	Button openingScreenButton;
+	HashMap<String, Scene> sceneMap = new HashMap<>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,10 +33,43 @@ public class WordGuessClient extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("(Client) Word Guess!!!");
-		//Ronaldo push test
-		Scene scene = new Scene(new HBox(),600,600);
-		primaryStage.setScene(scene);
+
+		// set up start screen:
+		portBox = new TextField();
+
+		// prevent user from being able to enter more than 4 characters for port
+		portBox.setTextFormatter(new TextFormatter<String>(change ->
+				change.getControlNewText().length() <= 4 ? change : null));
+
+		// textbox for entering port
+		portText = new Text("Enter a port:");
+		portText.setFont(Font.font ("Verdana", 20));
+		portText.setStyle("-fx-font-weight: bold");
+		portText.setFill(Color.WHITE);
+
+		// textbox for entering ip address
+		ipBox = new TextField();
+		ipText = new Text("Enter an IP:");
+		ipText.setFont(Font.font ("Verdana", 20));
+		ipText.setStyle("-fx-font-weight: bold");
+		ipText.setFill(Color.WHITE);
+
+		openingScreenButton = new Button("Start Guess the Word!");
+
+		Pane startPane = new Pane();
+		startPane.setBackground(new Background(new BackgroundImage(new Image("randomLetters.png", 532, 720, false,true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,  BackgroundSize.DEFAULT)));
+		startPane.getChildren().addAll(ipText, ipBox, portText, portBox, openingScreenButton);
+		portText.relocate(185, 200);
+		portBox.relocate(165,230);
+		ipText.relocate(190,300);
+		ipBox.relocate(165, 330);
+		openingScreenButton.relocate(170, 400);
+
+
+		// show the start screen
+		sceneMap.put("start screen", new Scene(startPane, 532, 720));
+		primaryStage.setScene(sceneMap.get("start screen"));
 		primaryStage.show();
-	} // Sam pushes this boi
+	}
 
 }
