24ae16002a61716fae635ed23b0373cc072501fc
rmiguel3 <Muhammad878!>
2020-04-27 21:47:01 -0500

    created the guessing scene for the Client GUI

--- P4Client/src/main/java/WordGuessClient.java
+++ P4Client/src/main/java/WordGuessClient.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -23,11 +24,14 @@ public class WordGuessClient extends Application {
 	Text portText;
 	TextField ipBox;
 	Text ipText;
+	TextField letterGuessBox;
+	Text letterText;
 	Pane mainScenePane;
 	Button openingScreenButton;
 	Button gameButton;
 	Button sportsButton;
 	Button foodButton;
+	Button guessButton;
 	GuessClient clientConnection;
 	HashMap<String, Scene> sceneMap = new HashMap<>();
 	ListView<String> listItems2;
@@ -50,6 +54,7 @@ public class WordGuessClient extends Application {
 		portBox.setTextFormatter(new TextFormatter<String>(change ->
 				change.getControlNewText().length() <= 4 ? change : null));
 
+
 		// textbox for entering port
 		portText = new Text("Enter a port:");
 		portText.setFont(Font.font ("Verdana", 20));
@@ -63,10 +68,23 @@ public class WordGuessClient extends Application {
 		ipText.setStyle("-fx-font-weight: bold");
 		ipText.setFill(Color.WHITE);
 
+		// buttons for the first two scenes
 		openingScreenButton = new Button("Start Guess the Word!");
-		gameButton = new Button("Games");
+		gameButton = new Button("Video Games");
 		sportsButton = new Button("Sports");
 		foodButton = new Button("Food");
+
+		// all the necessary Texts, TextFields, and buttons needed for the game scene
+		letterGuessBox = new TextField();
+		letterText = new Text();
+		guessButton = new Button("Take your guess!");
+		listItems2 = new ListView<String>();
+		listItems2.setPrefSize(800,100);
+		letterGuessBox.setPrefSize(25,10);
+
+		// prevent user from being able to enter more than 1 character for a guess
+		letterGuessBox.setTextFormatter(new TextFormatter<String>(change ->
+				change.getControlNewText().length() <= 1 ? change : null));
 
 		//sets up the start screen
 		Pane startPane = new Pane();
@@ -77,6 +95,7 @@ public class WordGuessClient extends Application {
 		ipText.relocate(190,300);
 		ipBox.relocate(165, 330);
 		openingScreenButton.relocate(170, 400);
+		sceneMap.put("start screen", new Scene(startPane, 532, 720));
 
 		//Choosing category scene set up
 		mainScenePane = new Pane();
@@ -91,6 +110,14 @@ public class WordGuessClient extends Application {
 		gameButton.relocate(10, 50);
 		sportsButton.relocate(10, 180);
 		foodButton.relocate(10,310);
+		sceneMap.put("main screen", new Scene(mainScenePane,400,470));
+
+		//The guessing portion of the screen set up
+		Pane gameScreenPane = new Pane();
+		gameScreenPane.getChildren().addAll(guessButton, letterGuessBox, letterText, listItems2);
+		guessButton.relocate(400, 475);
+		letterGuessBox.relocate(445, 450);
+		listItems2.relocate(100,725);
 
 
 		// "Start Guess the Word!" button
@@ -106,10 +133,37 @@ public class WordGuessClient extends Application {
 				}, Integer.parseInt(portBox.getText()), ipBox.getText());
 				clientConnection.start();
 
-				sceneMap.put("main screen", new Scene(mainScenePane,400,470));
 				primaryStage.setScene(sceneMap.get("main screen"));
 			}
 		});
+
+		gameButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameScreenPane.setBackground(new Background(new BackgroundImage(new Image("videoGames.jpg", 1000, 900, false,true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,  BackgroundSize.DEFAULT)));
+				sceneMap.put("Game Screen", new Scene(gameScreenPane, 1000, 900));
+				primaryStage.setScene(sceneMap.get("Game Screen"));
+			}
+		});
+
+		sportsButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameScreenPane.setBackground(new Background(new BackgroundImage(new Image("sports.jpg", 1000, 900, false,true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,  BackgroundSize.DEFAULT)));
+				sceneMap.put("Game Screen", new Scene(gameScreenPane, 1000, 900));
+				primaryStage.setScene(sceneMap.get("Game Screen"));
+			}
+		});
+
+		foodButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameScreenPane.setBackground(new Background(new BackgroundImage(new Image("food.jpg", 1000, 900, false,true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,  BackgroundSize.DEFAULT)));
+				sceneMap.put("Game Screen", new Scene(gameScreenPane, 1000, 900));
+				primaryStage.setScene(sceneMap.get("Game Screen"));
+			}
+		});
+
 
 		//if window is closed the program exits out
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -121,9 +175,9 @@ public class WordGuessClient extends Application {
 		});
 
 		// show the start screen
-		sceneMap.put("start screen", new Scene(startPane, 532, 720));
 		primaryStage.setScene(sceneMap.get("start screen"));
 		primaryStage.show();
 	}
+
 
 }
